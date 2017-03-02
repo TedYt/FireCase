36cd85f5462b454e7cf7256536432182f361383b
TedYt <yutao.1987@outlook.com>
2017-03-02 14:07:56 +0800

    简单整理MainActivity文件

--- app/src/main/java/com/hytera/fcls/activity/MainActivity.java
+++ app/src/main/java/com/hytera/fcls/activity/MainActivity.java
@@ -12,7 +12,6 @@ import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
@@ -20,18 +19,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.hytera.fcls.service.FireService;
 import com.hytera.fcls.IMainAtv;
 import com.hytera.fcls.R;
-import com.hytera.fcls.mqtt.MQTT;
-import com.hytera.fcls.mqtt.event.MessageEvent;
 import com.hytera.fcls.presenter.MainAtvPresenter;
-
-import org.eclipse.paho.android.service.MqttAndroidClient;
-import org.eclipse.paho.client.mqttv3.MqttException;
-import org.greenrobot.eventbus.EventBus;
-import org.greenrobot.eventbus.Subscribe;
-import org.greenrobot.eventbus.ThreadMode;
+import com.hytera.fcls.service.FireService;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -41,28 +32,25 @@ public class MainActivity extends Activity implements IMainAtv {
 
     public static final String TAG = "y20650" + MainActivity.class.getSimpleName();
 
-    private static final String ServerIP = "192.168.43.22"; // 测试地址
-    private static final String PORTID = "1883"; // MQTT 协议的对应的端口
-    private static final String ClientID = "y20650";
     @BindView(R.id.textview)
     public TextView textView;
-
-
-
     @BindView(R.id.image_view)
     public ImageView imageView;
-
     @BindView(R.id.main_gridview_func)
     public GridView gridView;
     private MainAtvPresenter mainPresenter;
 
-    private static MqttAndroidClient client;
-    private MQTT mqtt;
     String[] fuc_names = new String[] { "拍照", "视频", "出发", "确认到达", "结束火警",
             "信息采集" };
-    int[] fuc_icons = new int[] { R.drawable.ic_launcher, R.drawable.ic_launcher,
-            R.drawable.ic_launcher, R.drawable.ic_launcher, R.drawable.ic_launcher, R.drawable.ic_launcher,
-            R.drawable.ic_launcher };
+    int[] fuc_icons = new int[] {
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,51 +65,13 @@ public class MainActivity extends Activity implements IMainAtv {
         startService(intent1);
 
         mainPresenter = new MainAtvPresenter(this, this);
-        //mainPresenter.initMQTT(this);
-        EventBus.getDefault().register(this); // 订阅消息总线
+
         initView();
     }
     //初始化布局资源
     private void initView() {
         gridView.setAdapter(new GridAdapter());
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                switch (i){
-                    case 0 :
-                        Toast.makeText(MainActivity.this, "照相", Toast.LENGTH_SHORT).show();
-                        //降低应用支持版本22，否则动态权限在华为7.0获取不成功
-                        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA )!= PackageManager.PERMISSION_GRANTED) {
-                            ActivityCompat.requestPermissions(MainActivity.this,
-                                    new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                                    2);
-                        }else {
-
-                            mainPresenter.startCamera(MainActivity.this);
-                        }
-
-                        break;
-                    case 1 :
-                        Toast.makeText(MainActivity.this, "视频", Toast.LENGTH_SHORT).show();
-                        break;
-                    case 2 :
-                        Toast.makeText(MainActivity.this, "出发", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(MainActivity.this,NaviActivity.class);
-                        startActivity(intent);
-                        break;
-                    case 3 :
-                        Toast.makeText(MainActivity.this, "确认到达", Toast.LENGTH_SHORT).show();
-                        break;
-                    case 4:
-                        Toast.makeText(MainActivity.this, "结束火警", Toast.LENGTH_SHORT).show();
-                        break;
-                    case 5 :
-                        Toast.makeText(MainActivity.this, "信息采集", Toast.LENGTH_SHORT).show();
-                        break;
-                }
-//                Toast.makeText(MainActivity.this, "ssd?dd+"+i+"====L?"+l, Toast.LENGTH_SHORT).show();
-            }
-        });
+        gridView.setOnItemClickListener(new GridViewOnItemClick());
     }
 
     @OnClick(R.id.textview)
@@ -134,13 +84,6 @@ public class MainActivity extends Activity implements IMainAtv {
             }
         }).start();
 
-//        mainPresenter.startCamera(this);
-        //startConnect(ClientID,ServerIP,PORTID);
-    }
-
-    private void initMQTT() {
-        //mqtt = new MQTT(this, this);
-        //mqtt.startConnect(this);
     }
 
 
@@ -157,10 +100,31 @@ public class MainActivity extends Activity implements IMainAtv {
         }
     }
 
-    @Subscribe(threadMode = ThreadMode.MAIN)
-    public void onMessageEvent(MessageEvent event){
-        Log.i(TAG, "getMessage from MQ : " + event.getString()
-                + ", topic is : " + event.getTopic());
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void updateLocation(double latitude, double longitude) {
+
+    }
+
+    @Override
+    public void showLogInMain(String s) {
+
+    }
+
+    private boolean checkCameraPermissoin(){
+        //降低应用支持版本22，否则动态权限在华为7.0获取不成功
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA )
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    2);
+            return false;
+        }
+        return true;
     }
 
     @Override
@@ -174,28 +138,6 @@ public class MainActivity extends Activity implements IMainAtv {
                     Toast.makeText(MainActivity.this, "权限未开启", Toast.LENGTH_SHORT).show();
                 }
         }
-    }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if(client!=null) {
-            try {
-                client.disconnect();
-            } catch (MqttException e) {
-                e.printStackTrace();
-            }
-        }
-        EventBus.getDefault().unregister(this);
-    }
-
-    @Override
-    public void updateLocation(double latitude, double longitude) {
-
-    }
-
-    @Override
-    public void showLogInMain(String s) {
-
     }
 
     class GridAdapter extends BaseAdapter {
@@ -217,56 +159,59 @@ public class MainActivity extends Activity implements IMainAtv {
 
           @Override
           public View getView(int i, View convertview, ViewGroup viewGroup) {
-            Holer holer;
+            Holder holder;
               if (convertview == null){
-                  holer = new Holer();
+                  holder = new Holder();
                   convertview = View.inflate(MainActivity.this,R.layout.main_function_item,null);
-                  holer.grid_item_text = (TextView) convertview.findViewById(R.id.grid_item_text);
-                  holer.grid_item_image = (ImageView) convertview.findViewById(R.id.grid_item_image);
+                  holder.grid_item_text = (TextView) convertview.findViewById(R.id.grid_item_text);
+                  holder.grid_item_image = (ImageView) convertview.findViewById(R.id.grid_item_image);
 
-                  convertview.setTag(holer);
+                  convertview.setTag(holder);
               }else{
-                  holer = (Holer) convertview.getTag();
+                  holder = (Holder) convertview.getTag();
               }
               //设置item的标题文本
-              holer.grid_item_image.setImageResource(fuc_icons[i]);
-              holer.grid_item_text.setText(fuc_names[i]);
+              holder.grid_item_image.setImageResource(fuc_icons[i]);
+              holder.grid_item_text.setText(fuc_names[i]);
               return convertview;
           }
       }
-    class Holer {
+    class Holder {
         TextView grid_item_text;
         ImageView grid_item_image;
     }
-    /*private void startConnect(String clientID, String serverIP, String port) {
-        //服务器地址
-        String  uri ="tcp://";
-        uri=uri+serverIP+":"+port;
-        Log.d(TAG, uri+"  "+clientID);
-        *//**
-         * 连接的选项
-         *//*
-        MqttConnectOptions conOpt = new MqttConnectOptions();
-        *//**设计连接超时时间*//*
-        conOpt.setConnectionTimeout(3000);
-        *//**设计心跳间隔时间300秒*//*
-        conOpt.setKeepAliveInterval(300);
-        *//**
-         * 创建连接对象
-         *//*
-        client = new MqttAndroidClient(this,uri, clientID);
-        *//**
-         * 连接后设计一个回调
-         *//*
-        client.setCallback(new MqttCallbackHandler(this, clientID));
-        *//**
-         * 开始连接服务器，参数：ConnectionOptions,  IMqttActionListener
-         *//*
-        try {
-            client.connect(conOpt, null, new ConnectCallBackHandler(this, imqConn));
-        } catch (MqttException e) {
-            e.printStackTrace();
-        }
 
-    }*/
+    class GridViewOnItemClick implements AdapterView.OnItemClickListener{
+
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            switch (i){
+                case 0 :
+                    Toast.makeText(MainActivity.this, "照相", Toast.LENGTH_SHORT).show();
+                    if (!checkCameraPermissoin()){
+                        return;
+                    }
+                    mainPresenter.startCamera(MainActivity.this);
+
+                    break;
+                case 1 :
+                    Toast.makeText(MainActivity.this, "视频", Toast.LENGTH_SHORT).show();
+                    break;
+                case 2 :
+                    Toast.makeText(MainActivity.this, "出发", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this,NaviActivity.class);
+                    startActivity(intent);
+                    break;
+                case 3 :
+                    Toast.makeText(MainActivity.this, "确认到达", Toast.LENGTH_SHORT).show();
+                    break;
+                case 4:
+                    Toast.makeText(MainActivity.this, "结束火警", Toast.LENGTH_SHORT).show();
+                    break;
+                case 5 :
+                    Toast.makeText(MainActivity.this, "信息采集", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    }
 }
