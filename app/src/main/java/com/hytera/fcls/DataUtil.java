9d2d894d26f151c628c2590970be0723e39a304c
TedYt <yutao.1987@outlook.com>
2017-03-06 14:53:59 +0800

    修改DataUtil文件，处理测试情况下的状态

--- app/src/main/java/com/hytera/fcls/DataUtil.java
+++ app/src/main/java/com/hytera/fcls/DataUtil.java
@@ -12,13 +12,34 @@ public class DataUtil {
     public static final String KEY_PASSWORD = "password";
     public static final String KEY_CHECKED = "checked_remember";
 
+    public static final String FIRE_CASE_URL =
+            "http://192.168.123.48:8080/icc_fcls/alarmStatus/reportStatus";
+
+    /** 接收警情 */
+    public static final int CASE_STATE_COPY = 1;
+    /** 出发 */
+    public static final int CASE_STATE_DEPART = 2;
+    /** 确认到达 */
+    public static final int CASE_STATE_ARRIVE = 3;
+    /** 结束警情 */
+    public static final int CASE_STATE_FINISH= 4;
+
+    /** 记录警情状态 */
+    public static int fireCaseState = 0;
+
+    /**
+     * 记录登录用户的信息
+     * 供上报服务器用
+     */
     private static LoginResponseBean loginResponseBean;
 
     public static void saveLoginResponseBean(LoginResponseBean bean){
         loginResponseBean = bean;
     }
 
-    public static LoginResponseBean.UserBean getLoginedUserBean(){
+    public static LoginResponseBean.UserBean getLoginUserBean(){
+        if (loginResponseBean == null) return null;
+
         return loginResponseBean.getUser();
     }
 }
