c3d94928ced3f5662026dcad01c63388ecf1a71a
sunnymooni <cctav12@gmail.com>
2017-03-07 19:29:56 +0800

    增加上传文件接口

--- app/src/main/java/com/hytera/fcls/presenter/NetUtils.java
+++ app/src/main/java/com/hytera/fcls/presenter/NetUtils.java
@@ -7,9 +7,14 @@ package com.hytera.fcls.presenter;
 import android.accounts.NetworkErrorException;
 import android.util.Log;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -88,6 +93,68 @@ public class NetUtils {
             }
         }
         return null;
+    }
+
+    /**
+     * 上传文件
+     * @param u
+     * @param filename
+     * @param filepath
+     */
+    public static void postFile(String u, String filename,String filepath) {
+        HttpURLConnection conn = null;
+        // boundary就是request头和上传文件内容的分隔符
+        String BOUNDARY = "---------------------------hytera";
+
+        try {
+            File f = new File(filepath);
+            URL url = new URL(u);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setConnectTimeout(50000);
+            conn.setReadTimeout(30000);
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setUseCaches(false);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Connection", "Keep-Alive");
+            conn.setRequestProperty("User-Agent",
+                    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:26.0) Gecko/20100101 Firefox/26.0");
+            conn.setRequestProperty("Accept-Language", "zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3");
+            conn.setRequestProperty("Accept-Encoding", "gzip, deflate");
+            conn.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+            conn.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + BOUNDARY);
+
+            OutputStream out = new DataOutputStream(conn.getOutputStream());
+            byte[] fb = new byte[(int) f.length()];
+            FileInputStream fis = new FileInputStream(f);
+            fis.read(fb);
+            StringBuffer strBuf = new StringBuffer();
+            String inputName = "uploadfile";
+            String contentType = "image/jpeg";
+            strBuf.append("\r\n").append("--").append(BOUNDARY).append("\r\n");
+            strBuf.append(
+                    "Content-Disposition: form-data; name=\"" + inputName + "\"; filename=\"" + filename + "\"\r\n");
+            strBuf.append("Content-Type:" + contentType + "\r\n\r\n");
+            out.write(strBuf.toString().getBytes());
+            out.flush();
+            out.write(fb);
+            out.flush();
+            out.write(("\r\n--"+BOUNDARY+"--\r\n").getBytes());
+            out.flush();
+            out.close();
+            fis.close();
+            strBuf = new StringBuffer();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                strBuf.append(line).append("\n");
+            }
+            Log.e(TAG, "post successfully");
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e(TAG, "post failed : " + e.getMessage());
+        }
+
     }
 
     private static String getStringFromInputStream(InputStream is) throws IOException{
